ff9b9b66300d8035364159ccf5016d5553a976a0
ahw26 <ahw26@duke.edu>
2019-06-06 15:37:57 -0400

    initial commit

---
+++ DukeEventAttendanceApp/DukeEventAttendanceApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DukeEventAttendanceApp
+//
+//  Created by Luiza Wolf on 6/6/19.
+//  Copyright © 2019 Duke OIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
