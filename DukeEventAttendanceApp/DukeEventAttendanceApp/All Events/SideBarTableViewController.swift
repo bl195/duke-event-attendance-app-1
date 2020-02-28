48f14d4f1aa9b31fcd52cb1467862a3f3a39f998
Brian Li <bl195@duke.edu>
2020-02-28 16:31:04 -0500

    refactoring calendar and side bar

--- DukeEventAttendanceApp/DukeEventAttendanceApp/All Events/SideBarTableViewController.swift
+++ DukeEventAttendanceApp/DukeEventAttendanceApp/All Events/SideBarTableViewController.swift
@@ -7,142 +7,37 @@
 //
 
 import UIKit
-import Apollo
-
-struct cellData {
-    var opened = Bool()
-    var sectionData = [String]()
-}
 
 class SideBarTableViewController: UITableViewController {
     
-    var menuFilterSelected: Bool = false
-    var didTapMenuType: ((String) -> Void)?
-    var filtername:String = ""
-    var thisDateCode = ""
-    var oAuthService: OAuthService?
-    var tableViewData = [cellData]()
-    
-    class Message{
-        
-        let summary: String
-        let pic: UIImage
-        
-        init(for summ: String, _ icon: UIImage){
-            self.summary = summ
-            self.pic = icon
-        }
-    }
-    
-    let datas: [Message] = [Message(for: "Date", UIImage(named: "icons8")!),  Message(for: "Topic", UIImage(named: "topics")!)]
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "FILTER BY:"
-        //self.navigationController?.navigationBar.titleView = "Filter By:"
-        self.navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.kern: 5.0, NSAttributedString.Key.foregroundColor: UIColor.white, NSAttributedString.Key.font: UIFont(name: "Helvetica", size: 35)]
         
-//         tableViewData = [cellData(opened: false, sectionData: ["Arts", "Athletics/Recreation", "Global Duke", "Civic Engagement/Social Action", "Diversity/Inclusion", "Energy", "Engineering", "Ethics", "Health/Wellness", "Humanities", "Natural Sciences", "Politics", "Religious/Spiritual", "Research", "Social Sciences", "Sustainability", "Teaching & Classroom Learning", "Technology", "University Events"])]
         
-//        oAuthService = OAuthService.shared
-       
-    }
-    
-    
-    @objc func tapButton(){
-        print("tapped")
+        //switch
+        let my_switch = UISwitch(frame: .zero)
+        my_switch.isOn = switchOn
+        my_switch.addTarget(self, action: #selector(switchToggled(_:)), for: .valueChanged)
+        let switch_display = UIBarButtonItem(customView: my_switch)
+        navigationItem.rightBarButtonItems = [cal, switch_display]
+        
+        
+        //settings bar
+        let settings_display = UIBarButtonItem(image: UIImage(named: "icons8-settings-24"), style: .done, target: self, action: #selector(tapButton))
+        
+        navigationItem.leftBarButtonItems = [topicfilter, settings_display]
+        
     }
     
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return 0
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-//        if tableViewData[section].opened == true{
-//            return tableViewData[section].sectionData.count + datas.count
-//        }
-//        else{
-        return datas.count
-//    }
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-//        if indexPath.row == 1 || indexPath.row == 0 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell") as! MessageCell
-            
-            cell.labelcell.text = datas[indexPath.row].summary
-            cell.imagecell.image = datas[indexPath.row].pic
-            
-            return cell
-        
-            
-//        else{
-//            guard let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell") else {return UITableViewCell()}
-//            cell.textLabel?.text = tableViewData[indexPath.section].sectionData[indexPath.row-3]
-//            return cell
-//        }
-        
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        if(indexPath.row == 1){
-                        let vc = self.storyboard?.instantiateViewController(withIdentifier: "MenuViewController") as? MenuViewController
-                        self.navigationController?.pushViewController(vc!, animated: true)
-            
-//            if tableViewData[indexPath.section].opened == true{
-//                tableViewData[indexPath.section].opened = false
-//                let sections = IndexSet.init(integer: indexPath.section)
-//                tableView.reloadSections(sections, with: .none)
-//            }else{
-//                tableViewData[indexPath.section].opened = true
-//                let sections = IndexSet.init(integer: indexPath.section)
-//                tableView.reloadSections(sections, with: .none)
-//            }
-        }
-        
-        if (indexPath.row == 0){
-            let vc = self.storyboard?.instantiateViewController(withIdentifier: "CalendarViewController") as? CalendarViewController
-            self.navigationController?.pushViewController(vc!, animated: true)
-        }
-        
-//        if(indexPath.row == 0){
-//
-//            let alert = UIAlertController(title: "Logout", message: "Do you want to Log out?", preferredStyle: .alert)
-//            let subButton = UIAlertAction(title: "Yes", style: .default, handler: { (action) -> Void in
-//                self.oAuthService?.logout()
-//                print (self.oAuthService?.isAuthenticated())
-//                let vc = self.storyboard?.instantiateViewController(withIdentifier: "mainNav") as? UINavigationController
-//                self.navigationController?.present(vc!, animated: true)
-//
-//                print("logged out")
-//
-//            })
-//            let cancelButton = UIAlertAction(title: "No", style: .cancel, handler: nil)
-//            alert.addAction(subButton)
-//            alert.addAction(cancelButton)
-//            self.present(alert, animated: true, completion: nil)
-////            if(subButton.isEnabled == true){
-////                oAuthService?.logout()
-////                let vc = self.storyboard?.instantiateViewController(withIdentifier: "ViewController") as? UIViewController
-////                self.navigationController?.present(vc!, animated: true)
-////
-////                print("logged out")
-////            }
-//        }
-//        if(indexPath.row > 2){
-//            let viewcontroller = storyboard?.instantiateViewController(withIdentifier: "EventTableViewController") as? EventTableViewController
-//            viewcontroller?.title = menuArray[indexPath.row-3].uppercased()
-//            viewcontroller?.filtername = menuArray[indexPath.row-3]
-//            viewcontroller?.encodedate = thisDateCode
-//            self.navigationController?.pushViewController(viewcontroller!, animated:true)
-//        }
-        
+        return 0
     }
     
 }
